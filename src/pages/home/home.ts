3ddba5e86c3a15ee7b9f773d86a7fae170af7504
Somyot <somyot.srihanon@gmail.com>
2018-02-27 16:27:38 +0700

    Fix bad repo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  userData: any;
 
+  constructor(public navCtrl: NavController,private facebook: Facebook) {
+
+  }
+
+  loginWithFB() {
+    this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
+      this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+        this.userData = {email: profile['email'], 
+        first_name: profile['first_name'], 
+        picture: profile['picture_large']['data']['url'], 
+        username: profile['name']}
+      });
+    });
   }
 
 }
